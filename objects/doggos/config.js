ed1a19aa8aec4409699e4fe138cd1033e6c44485
Joel Simpson <joel.simpson@gmail.com>
2021-07-23 16:52:47 -0700

    NEW: Adding m2_o1 and other changes

--- objects/doggos/config.js
+++ objects/doggos/config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  animations: {
-    default: {
-      frames: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
-      frameRate: 1,
-    },
-  },
   spriteSheets: {
-    doggos: {
+    cloudinary_doggo1: {
       fileName: "doggy1.png",
       frameDimensions: {
         width: 25,
@@ -14,11 +8,23 @@ module.exports = {
       },
     },
   },
-  properties: {
-    sprite: {
-      defaultFrameIndex: 0,
-      spriteSheet: "doggos",
-      layers: [],
+  animations: {
+    idle: {
+      frames: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
+      frameRate: 9,
     },
   },
+  events:{
+    onMapDidLoad:(self)=>{
+      self.playAnimation("idle");
+    }
+  }
+
+  // properties: {
+  //   sprite: {
+  //     defaultFrameIndex: 0,
+  //     spriteSheet: "doggos",
+  //     layers: [],
+  //   },
+  // },
 };
