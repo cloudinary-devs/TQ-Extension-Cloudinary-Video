d0383dbbcdcef36da8845f4e3e18c33c2c59d034
Joel Simpson <joel.simpson@gmail.com>
2021-08-03 13:50:36 -0700

    adding proper validation to m1_o2_sign_up

--- levels/cloudinary_video/objectives/m1_o2_sign_up/validator.js
+++ levels/cloudinary_video/objectives/m1_o2_sign_up/validator.js
@@ -6,7 +6,15 @@ const browser = require('../../../../lib/browser');
 module.exports = async function (helper) {
 
     try {
-        const {cloudinary_cloud_name, cloudinary_api_key, cloudinary_api_secret} = helper.validationFields;
+
+        const cloudinary_cloud_name = helper.getNormalizedInput('cloudinary_cloud_name');
+        const cloudinary_api_key = helper.getNormalizedInput('cloudinary_api_key');
+        const cloudinary_api_secret = helper.getNormalizedInput('cloudinary_api_secret');
+
+        if (!cloudinary_cloud_name || !cloudinary_api_key || !cloudinary_api_secret) {
+            helper.fail('Please complete the form and click HACK again');
+            return;
+        }
 
         cloudinary.config({
             cloud_name: cloudinary_cloud_name,
@@ -14,23 +22,31 @@ module.exports = async function (helper) {
             api_secret: cloudinary_api_secret
         });
 
-        http.get(cloudinary.url('sample'));
-        browser.show();
-        browser.display(`
-        <div style="width:100%;text-align: center;">
-            <h1>Pulling a sample image from your account.</h1>
-            <img src="${cloudinary.url('sample')}" alt="sample image">
-        </div>
-    `);
+        http.get(cloudinary.url('sample'),function(res){
+            if(res.statusCode !== 200){
+                helper.fail(`Sorry, that didn't work. Please check your information and try again.`);
+                return;
+            }
+            browser.show();
+            browser.display(`
+                <div style="width:100%;text-align: center;">
+                    <h1>Pulling a sample image from your account.</h1>
+                    <img src="${cloudinary.url('sample')}" alt="sample image">
+                </div>
+            `);
 
 
-        //This stores the values into environment variables of 'name'
-        helper.success(`Hooray! You did it!`, [
-            {name: 'CLOUDINARY_CLOUD_NAME', value: cloudinary_cloud_name},
-            {name: 'CLOUDINARY_API_KEY', value: cloudinary_api_key},
-            {name: 'CLOUDINARY_API_SECRET', value: cloudinary_api_secret, concealed: true}
-        ]);
-    }catch(e){
+            //This stores the values into environment variables of 'name'
+            helper.success(`Hooray! You did it!`, [
+                {name: 'CLOUDINARY_CLOUD_NAME', value: cloudinary_cloud_name},
+                {name: 'CLOUDINARY_API_KEY', value: cloudinary_api_key},
+                {name: 'CLOUDINARY_API_SECRET', value: cloudinary_api_secret, concealed: true}
+            ]);
+        });
+
+
+
+    } catch (e) {
         console.error(e);
     }
 };
