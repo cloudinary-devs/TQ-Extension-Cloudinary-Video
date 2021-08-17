89d66a1ed5b663a59ccc81f952e4e005a2ffab0f
Joel Simpson <joel.simpson@gmail.com>
2021-08-17 15:59:17 -0700

    Removing ffmpeg requirement

--- levels/cloudinary_video/objectives/m2_o1_uploading/validator.js
+++ levels/cloudinary_video/objectives/m2_o1_uploading/validator.js
@@ -1,6 +1,5 @@
 const browser = require('../../../../lib/browser');
 const download = require('../../../../lib/download');
-const ffmpeg = require('fluent-ffmpeg');
 
 module.exports = async function (helper) {
 
@@ -14,32 +13,15 @@ module.exports = async function (helper) {
     download(url1, 'cloudinary_m2_o1_temp.mp4')
         .then(
             (filename) => {
-                console.log(filename);
-                //Check the downloaded file to see if it looks like the right file
-                ffmpeg.ffprobe(filename, function (err, metadata) {
-                    if(err){
-                        console.log(err);
-                        helper.fail("Sorry, an error occurred"+err);
-                    }
-                    console.log(metadata);
-                    if (
-                        metadata.streams[0].coded_height === 368 &&
-                        metadata.streams[0].coded_width === 640
-                    ) {
-                        helper.success('Nice Work!');
-                        browser.display(`
-                            <div>
-                                <h1>Success!</h1>
-                                <video autoplay loop>
-                                    <source src="${url1}" type="video/mp4">
-                                </video>
-                            </div>`
-                        );
-                    } else {
-                        //@todo-p2 customize message on why it doesn't appear to be correct
-                        helper.fail("Sorry, that doesn't appear to be the correct asset.");
-                    }
-                });
+                helper.success('Nice Work!');
+                browser.display(`
+                    <div>
+                        <h1>Success!</h1>
+                        <video autoplay loop>
+                            <source src="${url1}" type="video/mp4">
+                        </video>
+                    </div>`
+                );
             }
         )
         .catch((e) => {
