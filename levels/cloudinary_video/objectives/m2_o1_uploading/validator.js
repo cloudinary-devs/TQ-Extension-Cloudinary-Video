ea327c76864f1d3f0b188e7f202c1760e607f36f
Joel Simpson <joel.simpson@gmail.com>
2021-10-09 16:10:27 -0700

    updating m2_o1 to new grader/downloader

--- levels/cloudinary_video/objectives/m2_o1_uploading/validator.js
+++ levels/cloudinary_video/objectives/m2_o1_uploading/validator.js
@@ -1,39 +1,26 @@
 const browser = require('../../../../lib/browser');
-const download = require('../../../../lib/download');
-const state = require('../../../../lib/state');
-const fs = require('fs');
+const Grader = require("../../../../lib/grader");
 
 module.exports = async function (helper) {
-    state.saveAnswers(helper);
-    // We start by getting the user input from the helper
-    const url1 = helper.getNormalizedInput('url1');
-
-    if (!url1) {
-        helper.fail("Please complete the form.");
-    }
-
-    download(url1, 'cloudinary_m2_o1_temp.mp4')
-        .then(
-            (filename) => {
-                //@todo validate file size
-                const fileSize = fs.statSync(filename).size;
-                const expectedFileSize = 676502;
-                if(fileSize !== expectedFileSize){
-                    return helper.fail(`This doesn't appear to be the correct file.  It should be ${expectedFileSize} bytes, but is ${fileSize} bytes. You should go back through the steps make sure you download the correct (smallest) version.`);
-                }
-
-                helper.success('Nice Work!');
-                browser.display(`
+    let grader = new Grader(helper, {
+        answer1: {
+            validExample: 'https://res.cloudinary.com/joelsimpson/video/upload/demo/kitten_fighting.mp4',
+            mustAppear: ['https:','res.cloudinary.com','video/upload','Succulent_-_14467','.mp4']
+        }
+    }, function pass() {
+        helper.success(grader.getSuccessMessage());
+        browser.display(`
+            <div>
+                <h1>Success!</h1>
+                <div style="display: flex;flex-wrap: wrap;justify-content: space-evenly">
                     <div>
-                        <h1>Success!</h1>
-                        <video autoplay loop>
-                            <source src="${url1}" type="video/mp4">
-                        </video>
-                    </div>`
-                );
-            }
+                        <video autoplay loop controls><source src="${grader.getVideoUrl('answer1')}" type="video/mp4"></video>
+                    </div>
+                </div>
+            </div>
+        `
         )
-        .catch((e) => {
-            helper.fail(e);
-        });
-};
+    });
+
+    grader.grade();
+}
